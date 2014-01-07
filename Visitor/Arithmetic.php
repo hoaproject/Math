582bad68a7ecd080c4f30942461014099fc456f9
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2014-01-07 11:52:22 +0100

    Happy new year \o/.

--- Visitor/Arithmetic.php
+++ Visitor/Arithmetic.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright © 2007-2013, Ivan Enderlin. All rights reserved.
+ * Copyright © 2007-2014, Ivan Enderlin. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -71,7 +71,7 @@ namespace Hoa\Math\Visitor {
  * @author     Sébastien Houze <s@verylastroom.com>
  * @author     Ivan Enderlin <ivan.enderlin@hoa-project.net>
  * @author     Cédric Dugat <cedric@dugat.me>
- * @copyright  Copyright © 2007-2013 Stéphane Py, Sébastien Houze,
+ * @copyright  Copyright © 2007-2014 Stéphane Py, Sébastien Houze,
  *             Ivan Enderlin, Cédric Dugat.
  * @license    New BSD License
  */
