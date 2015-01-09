7710a10a83a090e810bae97b0a5398c6659a93a0
Juha Remes <jremes@outlook.com>
2015-01-09 19:29:40 +0000

    Added constants to Arithmetic

--- Visitor/Arithmetic.php
+++ Visitor/Arithmetic.php
@@ -383,7 +383,14 @@ class Arithmetic implements Visitor\Visit {
                 'SQRT_PI' => M_SQRTPI,
                 'SQRT_2'  => M_SQRT2,
                 'SQRT_3'  => M_SQRT3,
-                'LN_PI'   => M_LNPI
+                'LN_PI'   => M_LNPI,
+                'LOG2E'   => M_LOG2E,
+                'LOG10E'  => M_LOG10E,
+                'LN2'     => M_LN2,
+                '1_PI'    => M_1_PI,
+                '2_SQRTPI' => M_2_SQRTPI,
+                '1_SQRT2'  => M_SQRT1_2,
+                'EULER'    => M_EULER
             ]);
 
         $this->_constants = $_constants;
