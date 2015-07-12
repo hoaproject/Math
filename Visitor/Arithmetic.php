2763f09cc4747bcfe7d73381f5c8f136dd913d75
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2015-07-12 22:12:33 +0200

    Add ^ operator for power.

--- Visitor/Arithmetic.php
+++ Visitor/Arithmetic.php
@@ -209,6 +209,16 @@ class Arithmetic implements Visitor\Visit
 
                 break;
 
+            case '#power':
+                $children[0]->accept($this, $a, $eldnah);
+
+                $acc = function ($b) use ($a, $acc) {
+                    return $acc(pow($a(), $b));
+                };
+
+                $children[1]->accept($this, $acc, $eldnah);
+                break;
+
             case '#fakegroup':
             case '#group':
                 $children[0]->accept($this, $a, $eldnah);
